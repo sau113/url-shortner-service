5665877bc14d533857d8e0d1534442803b80baf7
Saurab S <saurabs113@gmail.com>
2022-01-09 08:51:02 +0530

    URL Shortner Service initialized with spring starter dependencies - web, data jpa, h2 db

---
+++ src/main/java/com/example/projecturlshortner/ProjectUrlshortnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecturlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUrlshortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUrlshortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projecturlshortner/ProjectUrlshortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecturlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUrlshortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
